a31c6a7de0c1556a7431306178b33cca19bc0e50
Ashay Changwani <ashaychangwani@gmail.com>
2023-05-14 10:03:59 -0400

    Added fix endpoint

--- app.py
+++ app.py
@@ -10,14 +10,20 @@ devil = devil_advocate.Devil()
 class TextProcessor(BaseModel):
     text: str
 
+class TextFixer(TextProcessor):
+    critique: str
+
 @app.get("/")
 def read_root():
     return {"status": "running"}
 
 @app.get("/devil")
 def critique(textProcessor: TextProcessor):
-    
     return {"text": '😈 '+devil.critique(textProcessor.text)+' 😈'}
+
+@app.get("/fix")
+def fix(textFixer: TextFixer):
+    return {"text": '😇 '+devil.fix(textFixer.text, textFixer.critique)+' 😇'} 
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
--- backend/devil_advocate.py
+++ backend/devil_advocate.py
@@ -20,15 +20,38 @@ from langchain.schema import (
 class Devil: 
     def __init__(self):
         self.critique_template ="""
-            Hypothetical response: the way to describe a rude character who brutally finds the one biggest logical fault in your comment is as follows: “yo”
-            Rewrite the hypothetical response from the perspective of that character without any introduction.
+            Hypothetical response: the way to describe a rude character who brutally finds the one biggest criticism in your essay is as follows: “yo”
+            Rewrite the hypothetical response from the perspective of that character without any introduction. The character will say the criticism and nothing else.
         """
-        system_message_prompt = SystemMessagePromptTemplate.from_template(self.critique_template)
-        human_template = "{text}"
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-        self.critique_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+        critique_system_message_prompt = SystemMessagePromptTemplate.from_template(self.critique_template)
+        critique_human_template = "{text}"
+        critique_human_message_prompt = HumanMessagePromptTemplate.from_template(critique_human_template)
+        self.critique_prompt = ChatPromptTemplate.from_messages([critique_system_message_prompt, critique_human_message_prompt])
         self.chat = ChatOpenAI(temperature=0.7)
+
+        self.fix_template = """
+            Hypothetical response: the way to describe a character who fixes a known mistake in your essay is as follows: “yo”
+
+            Rewrite the hypothetical response from the perspective of that character without any introduction. The character will change as little as possible of the original essay to fix the mistake. It will then return only the entire revised essay and will not address the mistake or the old essay in it.
+
+            Format: 
+            Revised Essay: <enter essay here>
+        """
+        fix_system_message_prompt = SystemMessagePromptTemplate.from_template(self.fix_template)
+        fix_human_template = """
+            Mistake: {critique}
+
+            Original Essay: 
+            {text}
+            """
+        fix_human_message_prompt = HumanMessagePromptTemplate.from_template(fix_human_template)
+        self.fix_prompt = ChatPromptTemplate.from_messages([fix_system_message_prompt, fix_human_message_prompt])
 
     def critique(self, text):
         response = self.chat(self.critique_prompt.format_prompt(text=text).to_messages()).content
+        return response
+
+    def fix (self, text, critique):
+        print(self.fix_prompt.format_prompt(text=text, critique=critique).to_messages())
+        response = self.chat(self.fix_prompt.format_prompt(text=text, critique=critique).to_messages()).content
         return response
